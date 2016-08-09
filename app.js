d07705b716211c9a621c8a82e8d3584b5a74e0a8
ixistic <teeravipark@gmail.com>
2016-08-09 10:28:36 +0700

    change port environment

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ var express = require('express');
 var PokemonGO = require('pokemon-go-node-api')
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+    // ejs render automatically looks in the views folder
+    // res.render('index');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
